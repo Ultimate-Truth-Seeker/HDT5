ede655a070f53c5be35cd88f6122c03521530980
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-02-27 21:44:54 -0600

    Programa arreglado

--- HDT5.py
+++ HDT5.py
@@ -1,94 +1,54 @@
 import simpy
 import random
 
-""" Programa para simulación de uso de CPU, por Roberto Nájera """
-
-randomSeed = 1 # Common seed for random generation
-processes = 25 # Total processes
-EXP = 10 # parameter for exponential growth
+randomSeed = 10 # Common seed for random generation
+processes = 100 # Total processes
+EXP = 1 # parameter for exponential growth
 CPUinst = 3
+processors = 1
 MINmem = 1
 MAXmem = 10
 MINinst = 1
 MAXinst = 10
 
-def main():
-    random.seed(randomSeed)
-    env = simpy.Environment()
-    
-    
-    cpu = CPU(env)
-   # env.process(cpu.processCPU(env))
-    env.run()
-    
-class task:
-    def __init__(self, i, env):
-        self.name = "Process" + str(i)
-        self.m = random.randint(MINmem, MAXmem)
-        self.inst = random.randint(MINinst, MAXinst)
-        print("New " + self.name + " at " + str(env.now) + ", memory: " + str(self.m) + ", inst: " +str(self.inst))
+env = simpy.Environment()
+ram = simpy.Container(env, 100, 100)
+CPUs = simpy.Resource(env, processors)
 
-    def add(self, env, RAM, waiting, ready):
-        if RAM.level < self.m:
-            waiting.append(self)
-            print(self.name + " added to waiting stack at " + str(env.now))
-        else:
-            RAM.get(self.m)
-            ready.append(self)
-            print(self.name + " added to ready stack at " + str(env.now))
-            
-
-class CPU:
-    def __init__(self, env):
-        self.RAM = simpy.Container(env, 100, 100)
-        self.waiting = []
-        self.ready = []
-        self.tasking = env.process(self.genTasks(env))
-        
-
-    def genTasks(self, env):
-        for i in range(processes):
-            p = task(i, env)
-            p.add(env, self.RAM, self.waiting, self.ready)
-            env.process(self.processCPU(env))
+def generatePrograms(env):
+    for i in range(processes):
+            print(str(env.now) + " New program " + str(i))
+            env.process(computeProgram(env, i, 0))
             yield env.timeout(random.expovariate(1.0/EXP))
-            
+    print(env.now)
 
-    def processCPU(self, env):
-        
-            while len(self.ready) > 0:
-                p = self.ready.pop()
-                self.RAM.put(p.m)
-                p.inst -= 3
-                yield env.timeout(1)
-                if p.inst > 0:
-                    x = random.randint(1, 2)
-                    if x == 1:
-                        print(p.name + " added to waiting stack at " + str(env.now))
-                        self.waiting.append(p)
-                    elif x == 2:
-                        print(p.name + " readded to ready at " + str(env.now))
-                        self.ready.append(p)
-                        self.RAM.get(p.m)
-                else:
-                    print(p.name + " ended at " + str(env.now))
-                if len(self.waiting) != 0:
-                    while self.waiting[len(self.waiting) - 1].m <= self.RAM.level:
-                        self.RAM.get(self.waiting[len(self.waiting) - 1].m)
-                        self.ready.append(self.waiting.pop())
-                        print(self.ready[len(self.ready) - 1].name + " added to ready stack at " + str(env.now))
-                        if len(self.waiting) == 0:
-                            break
-                
-            
-                
-            
-            
+
+def computeProgram(env, i, n, m = random.randint(MINmem, MAXmem), inst = random.randint(MINinst, MAXinst)):
+    if n == 0:
+        print(str(env.now) + " Program " + str(i) + " requesting for memory: " + str(m))
+        yield ram.get(m)
+    elif n == 1:
+        print(str(env.now) + " Program " + str(i) + " waiting doing I/O")
+        yield env.timeout(random.randint(1, 10)) # Simula operaciones de entrada y salida
+
+    print(str(env.now) + " Program " + str(i) + " ready")
+    req = CPUs.request()
+    yield req
+    print(str(env.now) + " Program " + str(i) + " running inst: " + str(inst))
+    inst -= CPUinst
+    yield env.timeout(1)
+    CPUs.release(req)
+    if inst > 0:
+        decition = random.randint(1, 2)
+        env.process(computeProgram(env, i, decition, m, inst))
+    else:
+        yield ram.put(m)
+        print(str(env.now) + " Program " + str(i) + " terminated")
     
-
-
-
-main()
     
     
 
+env.process(generatePrograms(env))
+
+
+env.run()
